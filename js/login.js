ba3b523d91ea987d550cbed6ea1688e14bfbc9f3
Shahjalal-Fahad <shahjalalptw@gmail.com.com>
2021-08-16 20:12:38 +0600

    normal and founction counting

--- js/login.js
+++ js/login.js
@@ -8,7 +8,11 @@ document.getElementById('submit-button').addEventListener('click',function(){
     const userPassword=passwordField.value;
 
     if(userEmail=='fahad@gmail.com' && userPassword=='fahad'){
-        window.location.href='banking.html';
+        // normal counting
+        // window.location.href='banking.html';
+
+        // fouctionalities counting
+        window.location.href='bankingCount.html';
     }
 
 });
